0d51aeb004457e76300904c864196b9ec7f1bb45
Lucas Alves Pinheiro <l.pinheiro.w@gmail.com>
2026-01-08 11:31:03 -0300

    feat: adiciona estilização da hero

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,17 @@
 function HeroImg() {
-  return (
-		<section className="bg-pink-200 w-full h-150 flex justify-center">
-			<button className="self-end pb-30"> CTA </button>
+	return (
+		<section className="hero w-full h-150 flex">
+			<div className="self-end pl-40 pb-30 flex flex-col gap-2">
+				<h1 className="text-5xl font-bold">
+					Peças Únicas <br /> Para Momentos Únicos
+				</h1>
+				<p className="text-xl"> Feitas sob medida</p>
+				<button className="self-start border-amber-300 border text-white rounded-xl px-4 py-3">
+					<p className="font-bold text-2xl"> Encomende a Sua </p>
+				</button>
+			</div>
 		</section>
 	);
 }
 
-export default HeroImg
+export default HeroImg;
