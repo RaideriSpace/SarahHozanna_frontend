1816f4bfa6121cc2dbf69cd3551b614ec485c5db
Lucas Alves Pinheiro <l.pinheiro.w@gmail.com>
2026-01-08 17:00:49 -0300

    feat: estiliza mais a navbar e cria components.css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,20 +2,22 @@ import logo from "../assets/logo.svg";
 
 function Navbar() {
 	return (
-		<header className="w-full max-w-387.5 justify-self-center md:px-15 grid grid-cols-3 items-center">
+		<header className="w-full max-w-387.5 justify-self-center md:px-20 grid grid-cols-3 items-center">
 			<nav className="cols-start-1 pt-8">
-				<ul className="flex gap-4">
-					<li> Quem Sou </li>
-					<li> Peças </li>
-					<li> Sob Medida </li>
-					<li> Políticas </li>
+				<ul className="flex gap-6">
+					<li className="menu__link text-gray-600 before:bg-pink-200"> Quem Sou </li>
+					<li className="menu__link text-gray-600 before:bg-pink-200"> Peças </li>
+					<li className="menu__link text-gray-600 before:bg-pink-200"> Sob Medida </li>
+					<li className="menu__link text-gray-600 before:bg-pink-200"> Políticas </li>
 				</ul>
 			</nav>
 
 			<img src={logo} alt="Logo" className="cols-start-2 justify-self-center w-37" />
 
-			<div className="cols-start-3 justify-self-end pt-8">
-				<button> Contato </button>
+			<div className="cols-start-3 justify-self-end pt-4">
+				<button className="contact text-gray-600 before:bg-pink-200">
+					<span>Contato</span>
+				</button>
 			</div>
 		</header>
 	);
