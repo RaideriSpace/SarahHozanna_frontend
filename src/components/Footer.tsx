513652e1b56e5aea41a66b02e1e52c2c001b1602
Lucas Alves Pinheiro <l.pinheiro.w@gmail.com>
2026-01-20 17:22:18 -0300

    fix: ajusta link e tamanho da logo no footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,17 +7,17 @@ function Footer() {
 	return (
 		<footer>
 			<div className="h-70 w-full grid grid-cols-3">
-				<div className="col-start-2 self-center justify-self-center">
+				<div className="col-span-3 md:col-span-1 md:col-start-2 self-center justify-self-center">
 					<img src={logo} alt="Logo escrito Sarah Hozanna estilizado com um manequim e uma agulha" className="w-80" />
 					<div className="flex justify-center gap-4 pt-2">
 						<a href="https://www.instagram.com/sarahhozanna" rel="noopener noreferrer" target="_blank">
-							<ImInstagram size={30} className="transition-all hover:text-pink-700"/>
+							<ImInstagram size={30} className="transition-all hover:text-pink-700" />
 						</a>
-						<a href="https://wa.me/5511989157255" rel="noopener noreferrer" target="_blank">
-							<PiWhatsappLogoDuotone size={30} className="transition-all hover:text-pink-700"/>
+						<a href="https://wa.me/5511973314888" rel="noopener noreferrer" target="_blank">
+							<PiWhatsappLogoDuotone size={30} className="transition-all hover:text-pink-700" />
 						</a>
 						<a href="https://www.facebook.com/sarahsillysad" rel="noopener noreferrer" target="_blank">
-							<ImFacebook2 size={30} className="transition-all hover:text-pink-700"/>
+							<ImFacebook2 size={30} className="transition-all hover:text-pink-700" />
 						</a>
 					</div>
 				</div>
