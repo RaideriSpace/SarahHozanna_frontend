0128d030fa38be18bdab59e1d6cb59edb5675c27
Lucas Alves Pinheiro <l.pinheiro.w@gmail.com>
2026-02-04 12:05:42 -0300

    fix: corrige nome do feedback

--- src/data/avaliationsData.ts
+++ src/data/avaliationsData.ts
@@ -13,7 +13,7 @@ export const AllAvaliations: Avaliations[] = [
 		text: "Tive a honra de desfilar no SENAI Têxtil e Moda com um vestido de noiva feito do inicio ao fim pela Sarah. Sua atuação profissional destaca-se pelo rigor em cada etapa desde a precisão das medidas ao refinamento dos últimos detalhes. O vestido, de estilo romântico, foi desenvolvido com uma responsabilidade técnica admirável, resultando em uma peça que une estética e caimento perfeito. Um trabalho que reafirma a importância da dedicação e do talento na construção de uma moda autêntica, bem executada, personalizada e exclusiva para minha medida. Foi incrível ver de pertinho a construção da peça nos detalhes e acompanhar o tamanho desempenho da Sarah!",
 	},
 	{
-		nome: "Thalita",
+		nome: "Thainara",
 		modelo: "Vestido de Festa",
 		nota: 5,
 		text: "Amei a peça! Serviu certinho em mim, o acabamento é impecável e o material é de ótima qualidade. Dá pra ver o cuidado em cada detalhe. Com certeza usarei novamente e recomendo muito 💖",
